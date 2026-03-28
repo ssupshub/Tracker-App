8da465e14ea1276ae5595ac36120d4210783e919
Shubham <140893209+ssupshub@users.noreply.github.com>
2026-03-28 22:03:07 +0530

    feat: implement User model with password hashing and database-agnostic proxy fallback

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const { isDBConnected } = require('../config/db');
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -15,7 +16,7 @@ const UserSchema = new mongoose.Schema({
   },
 }, { timestamps: true });
 
-// Hash password before saving
+// Hash password before saving (Mongoose specific)
 UserSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
     return next();
@@ -25,9 +26,58 @@ UserSchema.pre('save', async function (next) {
   next();
 });
 
-// Compare password
+// Compare password (Mongoose specific)
 UserSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-module.exports = mongoose.model('User', UserSchema);
+const UserModel = mongoose.model('User', UserSchema);
+
+// ----------- IN-MEMORY FALLBACK ------------
+const memoryUsers = [];
+
+class MemoryUser {
+  static findOne({ username }) {
+    const user = memoryUsers.find((u) => u.username === username);
+    return Promise.resolve(user || null);
+  }
+
+  static findById(id) {
+    return {
+      select: function () {
+        return this; // chainable
+      },
+      then: function (resolve) {
+        const user = memoryUsers.find((u) => String(u._id) === String(id));
+        resolve(user || null);
+      },
+    };
+  }
+
+  static async create(data) {
+    const salt = await bcrypt.genSalt(Number(process.env.BCRYPT_SALT_ROUNDS) || 12);
+    const password = await bcrypt.hash(data.password, salt);
+    const newUser = {
+      _id: String(Date.now() + Math.random()),
+      username: data.username,
+      password,
+      matchPassword: async function (enteredPassword) {
+        return await bcrypt.compare(enteredPassword, this.password);
+      },
+    };
+    memoryUsers.push(newUser);
+    return newUser;
+  }
+}
+
+const UserProxy = new Proxy(UserModel, {
+  get(target, prop) {
+    if (!isDBConnected() && MemoryUser[prop]) {
+      return MemoryUser[prop].bind(MemoryUser);
+    }
+    return target[prop];
+  },
+});
+
+module.exports = UserProxy;
+
